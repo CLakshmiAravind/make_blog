aaf0d77bd19a33ed5811dc147f93e074a981efcb
CLakshmiAravind <n170848@rguktn.ac.in>
2024-03-22 00:58:53 +0530

    added sample.mdx file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import remarkUnwrapImages from 'remark-unwrap-images';
 import sitemap from '@astrojs/sitemap';
-
+import remarkAdmonitions from 'remark-admonitions';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx({
+		remarkPlugins: [
+		  remarkUnwrapImages
+		]
+	  }), sitemap()],
+	  markdownOptions: {
+		remarkPlugins: [
+		  remarkAdmonitions,
+		  // ...other plugins
+		],
+	  },
 });
